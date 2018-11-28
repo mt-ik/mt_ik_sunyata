302b3849613a1f6f7039deca321caeac1993f51b
xugy <18518165912@163.com>
2018-11-28 08:22:00 +0800

    menuButtom

--- lib/app/style/MKStyle.dart
+++ lib/app/style/MKStyle.dart
@@ -226,6 +226,8 @@ class MKICons {
     static const IconData MAIN_MINE = const IconData(0xe502, fontFamily: MKICons.FONT_FAMILY);
     static const IconData MAIN_SEARCH = const IconData(0xe61c, fontFamily: MKICons.FONT_FAMILY);
 
+    static const IconData GLOBAL_LOCATION = const IconData(0xe61c, fontFamily: MKICons.FONT_FAMILY);
+    
     static const IconData KARMA_SEARCH = const IconData(0xe60d, fontFamily: MKICons.FONT_FAMILY);
 
     static const IconData SOUL_EDIT = const IconData(0xe677, fontFamily: MKICons.FONT_FAMILY);
--- lib/app/view/soul/PurifySoul.dart
+++ lib/app/view/soul/PurifySoul.dart
@@ -93,31 +93,39 @@ class _TextInputState extends State<TextInput> {
 
     final TextEditingController controller = new TextEditingController();
    
+    String _bodyStr = "选项";
     @override
     Widget build(BuildContext context) {
         return new Container(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                    new Expanded(
-                        child: new TextField(
-                            controller: controller,
-                            keyboardType: TextInputType.multiline,
-                            decoration: InputDecoration(
-                                contentPadding: EdgeInsets.all(20.0),
-                                hintText: '开启真实的自己',
-                                border: InputBorder.none,
-                            ),
-                            maxLines: 100,
-                            onChanged: onChanged,
-                        ),
-                    ),
                     new Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+                        margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 0.0),
                         child: new Row(
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: <Widget>[
-                                new FlatButton(
+                                new Text(_bodyStr),
+                                new PopupMenuButton(
+                                    onSelected: (String value) {
+                                        setState(() {
+                                            _bodyStr = value;                                      
+                                        });
+                                    },
+                                    // initialValue: '选项一',
+                                    itemBuilder: (BuildContext context) => <PopupMenuItem<String>> [
+                                        new PopupMenuItem(
+                                            value:"选项一",
+                                            child: new Text("选项一")
+                                        ),
+                                        new PopupMenuItem(
+                                            enabled: true,
+                                            value: "选项二",
+                                            child: new Text("选项二")
+                                        ),
+                                    ]
+                                ),
+                                new FlatButton(  
                                     // color: Colors.black87,
                                     textColor: Colors.black54,
                                     child: new Text('添加地点'),
@@ -128,8 +136,45 @@ class _TextInputState extends State<TextInput> {
                             ],
                         ),
                     ),
+                    new Expanded(
+                        child: new TextField(
+                            controller: controller,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                                hintText: '开启真实的自己',
+                                border: InputBorder.none,
+                            ),
+                            maxLines: 100,
+                            onChanged: onChanged,
+                        ),
+                    ),
                     new Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+                        margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 0.0),
+                        child: new Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                                new FlatButton(
+                                    // color: Colors.black87,
+                                    textColor: Colors.black54,
+                                    child: new Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                            new Icon(MKICons.GLOBAL_LOCATION, size: 20.0),
+                                            new Text('定位')
+                                        ],
+
+                                    ),
+                                    onPressed: () {
+
+                                    },
+                                ),
+                            ],
+                        ),
+                    ),
+                    new Container(
+                        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 0, bottom: 5.0),
+                        // padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
                         decoration: new BoxDecoration(
                             border: new Border(top: new BorderSide( width: 1.2, color: Colors.black87)),
                         ),
