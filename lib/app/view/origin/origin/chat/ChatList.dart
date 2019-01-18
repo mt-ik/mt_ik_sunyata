78625117f129a469ff24bb7b35fe3c77f76a8255
xugy <18518165912@163.com>
2019-01-18 08:44:31 +0800

    头像

--- lib/app/view/origin/origin/chat/ChatList.dart
+++ lib/app/view/origin/origin/chat/ChatList.dart
@@ -7,129 +7,104 @@ class ChatList extends StatefulWidget {
 }
 
 class _ChatListState extends State<ChatList> {
+    static getChatList() {
+        List<Widget> list = [];
+        for (var i = 0; i < 5; i++) {
+            final chat = new FlatButton(
+                onPressed: () {
+                    print('object');
+                },
+                child: new Row(
+                    children: <Widget>[
+                        new Container(
+                            padding: EdgeInsets.only(right: 10),
+                            child: new ClipRRect(
+                                child: new FadeInImage.assetNetwork(
+                                    placeholder: 'assets/sunyata/mt_ik_tx.jpg',
+                                    fit: BoxFit.cover,
+                                    image: '',
+                                    height: 50,
+                                    width: 50,
+                                ),
+                                borderRadius: BorderRadius.circular(5.0)
+                            )
+                            // child: new ClipOval(
+                            //     child: new FadeInImage.assetNetwork(
+                            //         placeholder: 'assets/sunyata/mt_ik_tx.jpg',
+                            //         fit: BoxFit.cover,
+                            //         image: '',
+                            //         height: 50,
+                            //         width: 50,
+                            //     )
+                            // ),
+                            // child: new Image(
+                            //     height: 50,
+                            //     width: 50,
+                            //     image: new AssetImage('assets/sunyata/mt_ik_tx.jpg'),
+                            // ),
+                        ),
+                        new Expanded(
+                            child: new Container(
+                                height: 65,
+                                child: new Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                        new Expanded(
+                                            child: new Container(
+                                                height: 45,
+                                                child: new Row(
+                                                    children: <Widget>[
+                                                        new Column(
+                                                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                                            children: <Widget>[
+                                                                new Text('title' + i.toString()),
+                                                                new Text('time' + i.toString()),
+                                                            ],
+                                                        ),
+                                                    ],
+                                                ),
+                                            ),
+                                        ),
+                                        new Container(
+                                            margin: EdgeInsets.only(left: 10),
+                                            width: 50,
+                                            height: 60,
+                                            child: new Column(
+                                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                                children: <Widget>[
+                                                    // new Expanded(
+                                                        new Text('aa' + i.toString(), textAlign: TextAlign.right,),
+                                                    // ),
+                                                    // new Expanded(
+                                                        new Text('bbb' + i.toString(), textAlign: TextAlign.right,)
+                                                    // ),
+                                                ],
+                                            )
+                                        )
+                                    ],
+                                ),
+                                decoration: new BoxDecoration(
+                                    border: new Border(bottom: new BorderSide(
+                                        color: Colors.grey
+                                    ))
+                                ),
+                            ),
+                        ),
+                    ],
+                ),
+            );
+            list.add(chat);
+        }
+        return new Column(
+            children: list
+        );
+    }
+
     @override
     Widget build(BuildContext context) {
         return new Container(
-            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
             child: new Column(
-                children: <Widget>[
-                    new Container(
-                        child: new Row(
-                            children: <Widget>[
-                                new Container(
-                                    padding: EdgeInsets.only(right: 10),
-                                    child: new Image(
-                                        height: 50,
-                                        width: 50,
-                                        image: new AssetImage('assets/sunyata/mt_ik_tx.jpg'),
-                                    ),
-                                ),
-                                new Expanded(
-                                    child: new Container(
-                                        height: 65,
-                                        child: new Row(
-                                            mainAxisAlignment: MainAxisAlignment.start,
-                                            children: <Widget>[
-                                                new Expanded(
-                                                    child: new Container(
-                                                        height: 45,
-                                                        child: new Row(
-                                                            children: <Widget>[
-                                                                new Column(
-                                                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                                    children: <Widget>[
-                                                                        new Text('title'),
-                                                                        new Text('time')
-                                                                    ],
-                                                                ),
-                                                            ],
-                                                        ),
-                                                    ),
-                                                ),
-                                                new Container(
-                                                    margin: EdgeInsets.only(left: 10),
-                                                    width: 50,
-                                                    height: 50,
-                                                    child: new Column(
-                                                        children: <Widget>[
-                                                            new Expanded(
-                                                                child: new Text('aa'),
-                                                            ),
-                                                            new Text('bbb')
-                                                        ],
-                                                    )
-                                                )
-                                            ],
-                                        ),
-                                        decoration: new BoxDecoration(
-                                            border: new Border(bottom: new BorderSide(
-                                                color: Colors.grey
-                                            ))
-                                        ),
-                                    ),
-                                ),
-                            ],
-                        ),
-                    ),
-                    new Container(
-                        child: new Row(
-                            children: <Widget>[
-                                new Container(
-                                    padding: EdgeInsets.only(right: 10),
-                                    child: new Image(
-                                        height: 50,
-                                        width: 50,
-                                        image: new AssetImage('assets/sunyata/mt_ik_tx.jpg'),
-                                    ),
-                                ),
-                                new Expanded(
-                                    child: new Container(
-                                        height: 65,
-                                        child: new Row(
-                                            mainAxisAlignment: MainAxisAlignment.start,
-                                            children: <Widget>[
-                                                new Expanded(
-                                                    child: new Container(
-                                                        height: 45,
-                                                        child: new Row(
-                                                            children: <Widget>[
-                                                                new Column(
-                                                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                                    children: <Widget>[
-                                                                        new Text('title'),
-                                                                        new Text('time')
-                                                                    ],
-                                                                ),
-                                                            ],
-                                                        ),
-                                                    ),
-                                                ),
-                                                new Container(
-                                                    margin: EdgeInsets.only(left: 10),
-                                                    width: 50,
-                                                    height: 50,
-                                                    child: new Column(
-                                                        children: <Widget>[
-                                                            new Expanded(
-                                                                child: new Text('aa'),
-                                                            ),
-                                                            new Text('bbb')
-                                                        ],
-                                                    )
-                                                )
-                                            ],
-                                        ),
-                                        decoration: new BoxDecoration(
-                                            border: new Border(bottom: new BorderSide(
-                                                color: Colors.grey,
-                                            ))
-                                        ),
-                                    ),
-                                ),
-                            ],
-                        ),
-                    ),
-                ],
+                children: <Widget>[ getChatList() ],
             ),
         );
     }
